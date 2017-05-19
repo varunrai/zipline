69a5e31ccbd98bd768c67510dfae55e95cb525ca
Ana Ruelas <aruelas@quantopian.com>
2017-05-19 16:43:31 -0400

    ENH: Add function to calculate chunks within a date range

--- tests/utils/test_date_utils.py
+++ tests/utils/test_date_utils.py
@@ -4,10 +4,18 @@ from nose_parameterized import parameterized
 
 from zipline.testing import ZiplineTestCase
 from zipline.utils.calendars import get_calendar
-from zipline.utils.date_utils import roll_dates_to_previous_session
+from zipline.utils.date_utils import (
+    compute_date_range_chunks,
+    roll_dates_to_previous_session
+)
 
 
-class TestRollDatesToPreviousSession(ZiplineTestCase):
+class TestDateUtils(ZiplineTestCase):
+
+    @classmethod
+    def init_class_fixtures(cls):
+        super(TestDateUtils, cls).init_class_fixtures()
+        cls.calendar = get_calendar('NYSE')
 
     @parameterized.expand([
         (
@@ -24,6 +32,58 @@ class TestRollDatesToPreviousSession(ZiplineTestCase):
         ),
     ])
     def test_roll_dates_to_previous_session(self, date, expected_rolled_date):
-        calendar = get_calendar('NYSE')
-        result = roll_dates_to_previous_session(calendar, date)
-        self.assertEqual(result[0], expected_rolled_date)
+        self.assertEqual(
+            roll_dates_to_previous_session(self.calendar, date)[0],
+            expected_rolled_date
+        )
+
+    @parameterized.expand([
+        (
+            None,
+            [
+                (
+                    Timestamp('01-03-2017', tz='UTC'),
+                    Timestamp('01-31-2017', tz='UTC')
+                )
+            ]
+        ),
+        (
+            10,
+            [
+                (
+                    Timestamp('01-03-2017', tz='UTC'),
+                    Timestamp('01-17-2017', tz='UTC')
+                ),
+                (
+                    Timestamp('01-18-2017', tz='UTC'),
+                    Timestamp('01-31-2017', tz='UTC')
+                )
+            ]
+        ),
+        (
+            15,
+            [
+                (
+                    Timestamp('01-03-2017', tz='UTC'),
+                    Timestamp('01-24-2017', tz='UTC')
+                ),
+                (
+                    Timestamp('01-25-2017', tz='UTC'),
+                    Timestamp('01-31-2017', tz='UTC')
+                )
+            ]
+        ),
+    ])
+    def test_compute_date_range_chunks(self, chunksize, expected):
+        # These date ranges result in 20 business days
+        start_date = Timestamp('01-03-2017')
+        end_date = Timestamp('01-31-2017')
+
+        date_ranges = compute_date_range_chunks(
+            self.calendar,
+            start_date,
+            end_date,
+            chunksize
+        )
+
+        self.assertListEqual(list(date_ranges), expected)
--- zipline/utils/date_utils.py
+++ zipline/utils/date_utils.py
@@ -1,3 +1,6 @@
+from toolz import partition_all
+
+
 def roll_dates_to_previous_session(calendar, *dates):
     """
     Roll ``dates`` to the next session of ``calendar``.
@@ -11,11 +14,45 @@ def roll_dates_to_previous_session(calendar, *dates):
 
     Returns
     -------
-    rolled_dates: pandas.tseries.index.DatetimeIndex
+    rolled_dates: (np.datetime64, np.datetime64)
         The last trading date of the input dates, inclusive.
 
     """
     all_sessions = calendar.all_sessions
 
     locs = [all_sessions.get_loc(dt, method='ffill') for dt in dates]
-    return all_sessions[locs]
+    return all_sessions[locs].tolist()
+
+
+def compute_date_range_chunks(calendar, start_date, end_date, chunksize):
+    """Compute the start and end dates to run a pipeline for.
+
+    Parameters
+    ----------
+    calendar : TradingCalendar
+        The trading calendar to align the dates with.
+    start_date : pd.Timestamp
+        The first date in the pipeline.
+    end_date : pd.Timestamp
+        The last date in the pipeline.
+    chunksize : int or None
+        The size of the chunks to run.
+
+    Returns
+    -------
+    ranges : iterable[(np.datetime64, np.datetime64)]
+        A sequence of start and end dates to run the pipeline for.
+    """
+    if chunksize is None:
+        dates = roll_dates_to_previous_session(calendar, start_date, end_date)
+
+        return [(dates[0], dates[1])]
+
+    all_sessions = calendar.all_sessions
+    all_sessions.offset = None
+    start_ix, end_ix = all_sessions.slice_locs(start_date, end_date)
+    return (
+        (r[0], r[-1]) for r in partition_all(
+            chunksize, all_sessions[start_ix:end_ix]
+        )
+    )
